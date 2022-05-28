13641c1feeac46a4aa81160e952dc246e4c17ec5
Ervin Jahic <ervinjahic65@gmail.com>
2022-05-29 00:44:50 +0200

    added new section, brands

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { BrandsComponent } from './brands/brands.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule,
